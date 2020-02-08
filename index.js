ddb377cc9f145ff4b75c4758343a3b7be10b015f
Dan Petitt <danp@coderanger.com>
2020-02-08 13:38:39 +0000

    Resolve push command

--- index.js
+++ index.js
@@ -61,17 +61,15 @@ async function run() {
     const bf = new BadgeFactory();
     const svg = bf.create(format);
     await fs.writeFile(badgeFilePathInput, svg);
-
-    const files = fs.readdirSync(`.//`);
-    for (const file of files) {
-      core.info(`File: ${file}`);
-    }
   
     // Now commit the file
     core.info('Committing new badge');
     await exec.exec('git', ['config', 'user.name', `"${process.env['GITHUB_ACTOR']}"`]);
     await exec.exec('git', ['config', 'user.email', `"${process.env['GITHUB_ACTOR']}@users.noreply.github.com"`]);
-    await exec.exec('git', ['remote', 'set-url', 'origin', `"https://x-access-token:${repoTokenInput}@github.com/${process.env['GITHUB_REPOSITORY']}"`]);
+
+   await exec.exec('git', ['remote', 'set-url', 'origin', `https://x-access-token:${repoTokenInput}@github.com/`]);
+    // await exec.exec('git', ['remote', 'set-url', 'origin', `https://${GITHUB_ACTOR}:${repoTokenInput}@github.com/${process.env['GITHUB_REPOSITORY']}.git`]);
+
     await exec.exec('git', ['add', badgeFilePathInput]);
     await exec.exec('git', ['commit', '-m', `"Code coverage badge [skip ci]"`]);
     await exec.exec('git', ['push']);
